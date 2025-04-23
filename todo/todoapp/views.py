ac2660c8cbe5778795fe12087f70383ca4d29ddc
IliyanGeorgiev87 <iliyangeorgiev28@itpg-varna.bg>
2025-04-23 20:14:08 +0300

    register function

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -12,8 +13,19 @@ def register(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
 
-        new_user = User.objects.create_user(username=username, email=email, password=password)
-        new_user.save()
+        if len(password) < 4:
+            messages.error(request, 'Password is too short. Your password should be at least 4 characters.')
+            return redirect('register')
+        
+
+        # check if username already exist in DB
+        get_all_users_by_username = User.objects.filter(username=username)
+        if get_all_users_by_username:
+            messages.error(request, 'The username is already taken! Use another username.')
+            return redirect('register')
+        else:
+            new_user = User.objects.create_user(username=username, email=email, password=password)
+            new_user.save()
 
     return render(request, 'todoapp/register.html', {})
 
